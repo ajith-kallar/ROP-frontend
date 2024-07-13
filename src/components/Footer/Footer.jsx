7b92b444643fd5dbe8f85a19531a8dfa11bd5a2d
ajith-huho <ajithkskallar@outlook.com>
2024-07-13 13:37:53 +0530

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+      <div className='footer-content'>
+        <div className='footer-content-left'>
+          <img src={assets.logo} alt='' />
+          <p>
+            We craft exquisite biriyanis and sides, meticulously prepared to
+            tantalise your taste buds. As a cloud kitchen, we exclusively
+            partner with Swiggy and Zomato, ensuring seamless delivery of our
+            culinary treasures straight to your home.{" "}
+          </p>
+          <div className='footer-social-icons'>
+            <a href=''>
+              {" "}
+              <img src={assets.facebook_icon} alt='' />
+            </a>
+            <a href=''>
+              {" "}
+              <img src={assets.twitter_icon} alt='' />
+            </a>
+            <a href=''>
+              <img src={assets.linkedin_icon} alt='' />
+            </a>
+          </div>
+        </div>
+        <div className='footer-content-center'>
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+        <div className='footer-content-right'>
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>9044340704</li>
+            <li>hi@riceofpersia.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className='footer-copyright'>
+        Copyright 2024 © Riceofpersia.com - All Right Reserved.
+      </p>
+    </div>
+  );
+}
+
+export default Footer
